1167cb5ebf76432df80c9a42c84d6e0e8d00a2fd
EricksonRT <erik_lauri@hotmail.com>
2023-07-27 20:07:58 -0300

    Ajustes en card

--- src/components/CardEvent/CardEvent.jsx
+++ src/components/CardEvent/CardEvent.jsx
@@ -1,17 +1,16 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
 
 export default function CardEvent({ props }) {
   return (
-    <Grid container spacing={2} xs={12}>
-      <Grid item xs={12}>
-        <Card sx={{ maxWidth: 100+"%", height:"auto" }} key={props.slot.id}>
+    <Grid item xs={4}>
+      <Card sx={{ maxWidth: 100 + '%', height: 'auto' }} key={props.slot.id}>
         <CardMedia
           component="img"
           alt={props.slot.name}
@@ -23,9 +22,12 @@ export default function CardEvent({ props }) {
             {props.slot.name}
             {props.slot.emoji}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography variant="p" color="text.primary">
+            Inicio del evento:
             {props.startTime}
-            {props.endTime}
+          </Typography>
+          <Typography variant="body2" color="text.primary">
+            Cierre: {props.startTime}
           </Typography>
         </CardContent>
         <CardActions>
@@ -33,8 +35,6 @@ export default function CardEvent({ props }) {
           <Button size="small">Learn More</Button>
         </CardActions>
       </Card>
-    
-      </Grid>
     </Grid>
   );
 }
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import getEventsActives from '../Functions/Funtions';
 import Loading from '../Loading/Loading';
 import CardEvent from '../CardEvent/CardEvent';
+import { Grid } from '@mui/material';
 // Con la función de getEvent, podemos pasarle el estado de setEvents, y eso hará que nos setee los valores de los eventos en su función
 
 const Events = () => {
@@ -14,28 +15,30 @@ const Events = () => {
   return (
     <>
       <h1>Events</h1>
-      <div className="container">
-        <div className="component">
-          {events === null ? (
-            <Loading />
-          ) : (
-            events.map((event) => (
-              <CardEvent props={event}/>
-              // <div className="div" key={event.slot.id}>
-              //   {event.slot.name}
-              //   {event.slot.emoji}
-              //   {event.slot.hash}
-              //   {event.slot.listAlone}
-              //   {event.slot.hideable}
-              //   {event.slot.hideForSlot}
-              //   {event.slot.background}
-              //   {event.startTime}
-              //   {event.endTime}
-              // </div>
-            ))
-          )}
-        </div>
-      </div>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 9, md: 12 }}
+      >
+        {events === null ? (
+          <Loading />
+        ) : (
+          events.map((event) => (
+            <CardEvent props={event} />
+            // <div className="div" key={event.slot.id}>
+            //   {event.slot.name}
+            //   {event.slot.emoji}
+            //   {event.slot.hash}
+            //   {event.slot.listAlone}
+            //   {event.slot.hideable}
+            //   {event.slot.hideForSlot}
+            //   {event.slot.background}
+            //   {event.startTime}
+            //   {event.endTime}
+            // </div>
+          ))
+        )}
+      </Grid>
     </>
   );
 };
